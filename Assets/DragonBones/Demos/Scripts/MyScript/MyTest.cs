d6b6f58dc80ed7e3959fb7b2881cef5086a144f8
wuxucheng1111 <wuxucheng1111@gmail.com>
2019-11-30 01:27:39 +0900

    DragonBones测试用，测试场景MyScene，实现角色idle和walk动画的转换

---
+++ Assets/DragonBones/Demos/Scripts/MyScript/MyTest.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DragonBones;
+
+public class MyTest : MonoBehaviour
+{
+    private UnityArmatureComponent _armatureComp;
+    public float moveSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UnityFactory.factory.LoadDragonBonesData("mecha_2903/mecha_2903_ske");
+        UnityFactory.factory.LoadTextureAtlasData("mecha_2903/mecha_2903_tex");
+        this._armatureComp = UnityFactory.factory.BuildArmatureComponent("mecha_2903d");
+        this._armatureComp.animation.Play("idle");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+        if (x != 0 || y != 0)
+        {
+            _armatureComp.transform.Translate(x * moveSpeed, y * moveSpeed, 0, Space.World);
+            _armatureComp.transform.rotation = Quaternion.Euler(0, 0, (Vector2.SignedAngle(Vector2.right, new Vector2(x, y))));
+            Debug.Log(Vector2.SignedAngle(Vector2.right, new Vector2(x, y)));
+            if (x != 0)
+            {
+                //_armatureComp.transform.localScale = new Vector3(x, 1, 1);
+            }
+            if (_armatureComp.animation.lastAnimationName != "walk")
+            {
+                this._armatureComp.animation.FadeIn("walk", 0.2f);
+            }
+        }
+        else
+        {
+            if (_armatureComp.animation.lastAnimationName != "idle")
+            {
+                this._armatureComp.animation.FadeIn("idle", 0.2f);
+            }
+        }
+    }
+}
